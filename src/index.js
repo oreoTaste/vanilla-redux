863f6ab2953f024611d7144fde028ccf945f7bba
Youngkuk Sohn <youngkuk.sohn@gmail.com>
2020-08-16 10:58:32 +0900

    1.1 Store and Reducer

--- src/index.js
+++ src/index.js
@@ -1,23 +1,12 @@
+import { createStore } from "redux";
+
 const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-let count = 0;
-number.innerText = count;
-
-const updateText = () => {
-  number.innerText = count;
+const countModifier = (state = 0) => {
+  return state;
 };
+const countStore = createStore(countModifier);
 
-const handleAdd = () => {
-  count++;
-  updateText();
-};
-
-const handleMinus = () => {
-  count--;
-  updateText();
-};
-
-add.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
+console.log(countStore.getState());
