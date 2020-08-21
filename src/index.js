db64730b7330a0805d0d73c3d91f5994f708a89a
Youngkuk Sohn <youngkuk.sohn@gmail.com>
2020-08-21 22:18:16 +0900

    1.0 ~ 1.4 복습

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
-import {Provider} from "react-redux";
-import store from "./store";
+import { createStore } from "redux";
 
-ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider> ,
-    document.getElementById("root")
-);
+const plus = document.querySelector("#plus");
+const result = document.querySelector("#result");
+const minus = document.querySelector("#minus");
+
+const PLUS = "PLUS";
+const MINUS = "MINUS";
+
+const plusObject = {type: PLUS}
+const minusObject = {type: MINUS}
+
+const reducer = (state = 0, action) => {
+    switch(action.type) {
+        case PLUS:
+            return parseInt(state) + 1;
+        case MINUS:
+            return parseInt(state) - 1;
+        default:
+            return parseInt(state);
+    }
+}
+
+const store = createStore(reducer);
+
+store.subscribe(() => {
+    result.innerText = store.getState();
+});
+
+plus.addEventListener("click", () => {
+    store.dispatch(plusObject);
+})
+
+minus.addEventListener("click", () => {
+    store.dispatch(minusObject);
+})
+
